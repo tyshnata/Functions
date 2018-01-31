6907a7038a968003598baaa18e682782936e5817
tyshnata <tyshnata@gmail.com>
2018-01-31 13:53:06 +0200

    change of method start

--- src/Functions.java
+++ src/Functions.java
@@ -4,14 +4,64 @@ public class Functions extends Solutions{
 
     static Scanner scan = new Scanner(System.in);
     static int taskNumber; //номер задачи
+    static String startWord;
 
     public static void main(String[] args) {
 
         System.out.println("Привет!");
         System.out.println("Если хочешь найти ответы на некоторые вопросы, напиши *menu*, если нет напиши *exit*");
+        startWord = scan.next();
+        start(startWord);
+    }
 
-        start();
+    public static void start(String word){
+       switch (word){
+           case "menu":
+               choiceMenu();
+           break;
+           case "exit":
+               choiceExit();
+           break;
+           case "again":
+               choiceAgain();
+           break;
+           default:
+               choiceDefault();
+           break;
 
+       }
+    }
+
+    private static void  choiceMenu(){
+        functionMenu();
+        taskNumber =  scan.nextInt();
+        if (taskNumber>0 & taskNumber<9){
+            solvingProblems (taskNumber);// решаем задачу
+            menuOrExitOrAgain();// выбор - меню или выход - или решить эту задачу снова
+            startWord = scan.next();
+            start(startWord);
+        }
+        else{
+            erroneousData();
+            choiceMenu();
+        }
+    }
+
+    private static void  choiceAgain(){
+        solvingProblems (taskNumber);
+        menuOrExitOrAgain();
+        startWord = scan.next();
+        start(startWord);
+    }
+
+    private static void choiceExit(){
+        goodBye();
+    }
+
+    private static void choiceDefault(){
+        erroneousData();
+        startWord = scan.next();
+        start(startWord);
     }
 
     public static void functionMenu(){
@@ -26,45 +76,15 @@ public class Functions extends Solutions{
         System.out.println("8. Переписать строку в обратном порядке");
         System.out.println();
         System.out.println("Напишите номер задачи, которую Вы хотите решить");
-    }
 
-    public static void menuOrExitOrAgain(){
-        System.out.println();
-        System.out.println("Если хочешь решить такую задачу еще раз, напиши *again*.");
-        System.out.println("Если хочешь зайти в меню, напиши *menu*.");
-        System.out.println("Если хочешь выйти из программы, напиши *exit*.");
-
-    }
-
-    public static void start(){
-        if (scan.hasNext("menu")) { //выбор - меню
-            functionMenu();                 // показать меню
+        while (!scan.hasNextInt()){
+            erroneousData();
             scan.next();
-            taskNumber =  scan.nextInt(); //номер задачи, которую нужно решить
-            solvingProblems (taskNumber);// решаем задачу
-            menuOrExitOrAgain();        // выбор - меню или выход - или решить эту задачу снова
-            start();
         }
-        else {
-            if (scan.hasNext("exit")) {
-                goodBye();
-            } else {
-                if (scan.hasNext("again")){
-                    solvingProblems (taskNumber);
-                    menuOrExitOrAgain();
-                    scan.next();
-                    start();
-                }
-                else {
-                    erroneousData();
-                    scan.next();
-                    start();
 
-                }
-            }
-        }
+
+
     }
-
 
 }
 
