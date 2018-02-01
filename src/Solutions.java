166f6b58b9951cd095705261421cc20fd89d814a
tyshnata <tyshnata@gmail.com>
2018-02-01 11:14:50 +0200

    add. type matching check

--- src/Solutions.java
+++ src/Solutions.java
@@ -12,72 +12,73 @@ public class Solutions extends Mistakes{
             case 1:  // найдем наибольшее число в списке
                 solutionTask1();
                 break;
-
-           /* case 2:  // найдем число по индексу
-               solutionTask2();
-            break;*/
-
+            case 2:  // найдем число по индексу
+                 //solutionTask2();
+            break;
             case 3:
                solutionTask3();
             break;
-
             case 4:  // является ли строка палиндромом
                 solutionTask4();
             break;
-
             case 5:  //
                solutionTask5();
             break;
-
             case 6:  //  факториал числа
                 solutionTask6();
             break;
-
             case 7:   // найти индекс первого вхождения числа
                solutionTask7();
             break;
-
             case 8:  //
                solutionTask8();
             break;
-
             default:
             erroneousData();
-
         }
     }
 
-    private static void solutionTask1(){
+    private static void solutionTask1(){ //решение задачи 1
+        Integer[] listTask1;
+        int size1;
 
-        int size1 = 0;
         System.out.println("Сколько чисел в вашей последовательности?");
-        size1 = scan.nextInt();
-        Integer[] listTask1 = new Integer[size1]; //определяем, какой будет массив
-        System.out.println("Введите последовательность чисел через пробел:");
-
-        while (!scan.hasNextInt()) { // если элементы массива введены некорректно
+        while (!scan.hasNextInt()) {
             erroneousData();
             scan.next();
         }
-        for (int j = 0; j < size1; j++) {
-            listTask1[j] = scan.nextInt(); // заполняем массив числами с консоли
-        }
-        MaxNumber list1 = new MaxNumber(listTask1);
-        list1.answer();
-    }
-
-    /*private static void solutionTask2(){
-        List<Integer> listTask2 = new ArrayList<Integer>();
-
+        size1 = scan.nextInt();
+        listTask1 = new Integer[size1];
         System.out.println("Введите последовательность чисел через пробел:");
 
         while (!scan.hasNextInt()) {
             erroneousData();
             scan.next();
         }
+        for (int j = 0; j < size1; j++) {
+            listTask1[j] = scan.nextInt();
+        }
 
+        MaxNumber list1 = new MaxNumber(listTask1);
+        list1.answer();
+    }
 
+  /* private static void solutionTask2(){
+        List<Integer> listTask2 = new ArrayList<Integer>();
+        int k = 0;
+        System.out.println("Введите последовательность чисел через пробел:");
 
+        while (scan.hasNext()) {
+            if (scan.hasNextInt()){
+                listTask2.add(k,scan.nextInt());
+                k++;
+            }
+            else {
+                erroneousData();
+                solutionTask2();
+            }
+
+        }
 
         System.out.println("Введите индекс искомого числа:");
         Integer ind = scan.nextInt();
@@ -95,12 +96,16 @@ public class Solutions extends Mistakes{
             erroneousData();
             scan.next();
         }
+        if ((scan.nextInt()>0 ) & (scan.nextInt()<47)) {
+            num = scan.nextInt();
+            ElementFibonacci number = new ElementFibonacci(num);
+            number.answer();
+        }
 
-        num = scan.nextInt();
-        ElementFibonacci number = new ElementFibonacci(num);
-        number.answer();
-
-
+        else {
+            erroneousData();
+            solutionTask3();
+        }
     }
 
     private static void solutionTask4(){
@@ -148,11 +153,17 @@ public class Solutions extends Mistakes{
             scan.next();
         }
 
-        //if ((scan.nextInt()< 1) || (scan.nextInt()> 12));
+        if ((scan.nextInt()>0 ) & (scan.nextInt()<12)){
 
         num6 = scan.nextInt();
         FactorialOfANumber number6 = new FactorialOfANumber(num6);
         number6.answer();
+        }
+        else {
+            erroneousData();
+            solutionTask6();
+        }
+
     }
 
     private static void solutionTask7(){
@@ -161,6 +172,10 @@ public class Solutions extends Mistakes{
         Integer number7;
 
         System.out.println("Сколько чисел в вашей последовательности?");
+        while (!scan.hasNextInt()){
+            erroneousData();
+            scan.next();
+        }
         size7 = scan.nextInt();
 
         Integer[] listTask7 = new Integer[size7]; //определяем, какой будет массив
@@ -179,7 +194,6 @@ public class Solutions extends Mistakes{
             scan.next();
         }
         number7 = scan.nextInt();
-
         IndexOfANumber list7 = new IndexOfANumber(number7, listTask7);
         list7.answer();
     }
