68e2306fbc9a8dd1718a4cab4db14bf7119e5e3a
tyshnata <tyshnata@gmail.com>
2018-02-08 18:40:45 +0200

    the code block is placed in the class Text

--- src/Functions.java
+++ src/Functions.java
@@ -64,28 +64,6 @@ public class Functions extends Solutions{
         start(startWord);
     }
 
-    public static void functionMenu(){
-        System.out.println("Задачи:");
-        System.out.println("1. Найти наибольшее число в списке.");
-        System.out.println("2. Найти n-й элемент списка .");
-        System.out.println("3. Найти n-й элемент последовательности Фибоначчи ");
-        System.out.println("4. Определить, является ли строка палиндромом.");
-        System.out.println("5. Найти сумму двух чисел.");
-        System.out.println("6. Найти факториал заданного числа.");
-        System.out.println("7. Найти индекс первого вхождения заданного числа в список.");
-        System.out.println("8. Переписать строку в обратном порядке");
-        System.out.println();
-        System.out.println("Напишите номер задачи, которую Вы хотите решить");
-
-        while (!scan.hasNextInt()){
-            erroneousData();
-            scan.next();
-        }
-
-
-
-    }
-
 }
 
 
