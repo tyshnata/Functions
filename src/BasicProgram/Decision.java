f2fb63463054851abf4c6cc6bce4304cabeb1652
tyshnata <tyshnata@gmail.com>
2018-02-11 20:16:58 +0200

    delet file

--- src/BasicProgram/Decision.java
+++ src/BasicProgram/Decision.java
@@ -1,3 +1,5 @@
+package BasicProgram;
+
 public interface Decition {
     public void answer();
 }
---
+++ src/TestScanner/ScannerClass.java
@@ -0,0 +1,7 @@
+import java.util.Scanner;
+
+public class ScannerClass {
+    static Scanner scan = new Scanner(System.in);
+
+    public void inputValida
+}
