4b620941b1e46b73d67ea03d308b948967e716c3
tyshnata <tyshnata@gmail.com>
2018-02-11 20:38:29 +0200

    new class

--- src/TestScanner/ScannerClass.java
+++ src/TestScanner/ScannerClass.java
@@ -1,7 +1,39 @@
+package TestScanner;
+
 import java.util.Scanner;
 
-public class ScannerClass {
-    static Scanner scan = new Scanner(System.in);
+public class ScannerClass extends Text {
 
-    public void inputValida
+    private Scanner scan = new Scanner(System.in);
+
+    public Integer inputValidationNumber(){
+        Integer number;
+        while (!scan.hasNextInt()) {
+            erroneousData();
+            scan.next();
+        }
+        number = scan.nextInt();
+        return number;
+    }
+
+    public Integer[] inputValidationSequence(Integer sizeOfSequence){
+        Integer[] list;
+        list = new Integer[sizeOfSequence];
+        while (!scan.hasNextInt()) {
+            erroneousData();
+            scan.next();
+        }
+
+        for (int j = 0; j < sizeOfSequence; j++) {
+            list[j] = scan.nextInt();
+        }
+
+        return (list);
+    }
+
+    public String inputLine(){
+        String line;
+        line = scan.nextLine();
+        return line;
+    }
 }
