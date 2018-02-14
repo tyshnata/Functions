5bf31e9d73644bc544a0445a06eca0bdaa4e8801
tyshnata <tyshnata@gmail.com>
2018-02-14 19:27:16 +0200

    add fields

--- src/Constants.java
+++ src/Constants.java
@@ -1,6 +1,14 @@
-package UiUtils;
+
 
 public class Constants {
 
-    final int MAXIMUM_LIST_PROGRAM = 1;
+     final static int MAXIMUM_LIST_PROGRAM = 1,
+                     GET_ELEMENT_BY_INDEX_PROGRAM = 2,
+                     FIND_FIBONACCI_ELEMENT_PROGRAM = 3,
+                     CHECK_PALINDROM_PROGRAM = 4,
+                     SUM_OF_TWO_NUMBERS_PROGRAM = 5,
+                     FACTORIAL_PROGRAM = 6,
+                     NUMBER_INDEX_IN_THE_LIST_PROGRAM = 7,
+                     REVERS_LINE_PROGRAM = 8;
+
 }
